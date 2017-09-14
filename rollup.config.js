a4db04aa6b44baf8f8da93ecf3598adb1da8a942
Ben Southgate <bsouthga@gmail.com>
2017-09-14 18:57:11 -0400

    feat: add deploy script

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,30 @@ import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
 import minify from 'rollup-plugin-babel-minify';
 import typescript from 'rollup-plugin-typescript2';
+import * as fs from 'fs';
+import { execSync } from 'child_process';
+import * as CleanCSS from 'clean-css';
 
 const prod = process.env.NODE_ENV === 'production';
+
+function prodPlugin() {
+  return {
+    onwrite() {
+      const index = fs.readFileSync('./public/index.html').toString();
+      const redirect = fs.readFileSync('./public/redirect.html').toString();
+      const replaced = index.replace('<!--__REDIRECT__-->', redirect);
+      const input = fs.readFileSync('./public/styles.css');
+      const output = new CleanCSS({}).minify(input);
+      execSync('rm -rf ./public-gh-pages');
+      execSync('mkdir -p ./public-gh-pages');
+      execSync('cp ./public/* ./public-gh-pages');
+      execSync('rm ./public-gh-pages/redirect.html');
+      execSync('touch ./public-gh-pages/.nojekyll');
+      fs.writeFileSync('./public-gh-pages/index.html', replaced);
+      fs.writeFileSync('./public-gh-pages/styles.css', output.styles);
+    }
+  };
+}
 
 export default {
   input: 'src/index.ts',
@@ -12,7 +34,7 @@ export default {
     typescript(),
     resolve(),
     ...(prod
-      ? [minify({ comments: false })]
+      ? [minify({ comments: false }), prodPlugin()]
       : [
           serve({ contentBase: 'public', historyApiFallback: true }),
           livereload()
