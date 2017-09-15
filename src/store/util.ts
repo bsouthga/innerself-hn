15d0c3c5582e2295cbdebd5787106fc42fd8e37f
Ben Southgate <bsouthga@gmail.com>
2017-09-14 23:40:39 -0400

    cleanup: comments

--- src/store/util.ts
+++ src/store/util.ts
@@ -35,41 +35,26 @@ export const cachedFetch = (
     expiry = options;
     options = undefined;
   } else if (typeof options === 'object') {
-    // I hope you didn't set it to 0 seconds
     expiry = options.seconds || expiry;
   }
-  // Use the URL as the cache key to sessionStorage
   let cacheKey = `${STORAGE_PREFIX}${url}`;
   let cached = storage.getItem(cacheKey);
   let whenCached = storage.getItem(cacheKey + ':ts');
   if (cached !== null && whenCached !== null) {
-    // it was in sessionStorage! Yay!
-    // Even though 'whenCached' is a string, this operation
-    // works because the minus sign converts the
-    // string to an integer and it will work.
     let age = (Date.now() - Number(whenCached)) / 1000;
     if (age < expiry) {
       let response = new Response(new Blob([cached]));
       return Promise.resolve(response);
     } else {
-      // We need to clean up this old key
       storage.removeItem(cacheKey);
       storage.removeItem(cacheKey + ':ts');
     }
   }
 
   return fetch(url, options).then(response => {
-    // let's only store in cache if the content-type is
-    // JSON or something non-binary
     if (response.status === 200) {
       let ct = response.headers.get('Content-Type');
       if (ct && (ct.match(/application\/json/i) || ct.match(/text\//i))) {
-        // There is a .json() instead of .text() but
-        // we're going to store it in sessionStorage as
-        // string anyway.
-        // If we don't clone the response, it will be
-        // consumed by the time it's returned. This
-        // way we're being un-intrusive.
         response
           .clone()
           .text()
